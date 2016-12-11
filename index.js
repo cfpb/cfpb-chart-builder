c39b4dce08537f4d660297bc8bb49c8f2114433b
Chuck Werner <mistergone@gmail.com>
2016-12-11 16:16:19 -0500

    Fixes missing export, updates to version 0.0.5

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 var barChart = require( './src/charts/BarChart.js' );
 var tileMap = require( './src/charts/TileMap.js' );
+var lineChart = require( './src/charts/LineChart.js' );
 
 var chartBuilder = {
   barChart: barChart,
-  tileMap: tileMap
+  tileMap: tileMap,
+  lineChart: lineChart
 }
 
 module.exports = chartBuilder;

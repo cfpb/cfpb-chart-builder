e082d33c769ece3f32ab8a94aac5164878a8b459
Catherine Farman <Catherine.Farman@cfpb.gov>
2017-02-15 17:40:25 -0500

    add unit test files for utils js

---
+++ test/unit_tests/utils/get-color-scheme_test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var chai = require( 'chai' );
+var expect = chai.expect;
+var getColorScheme =
+  require( '../../../src/static/js/utils/get-color-scheme' );
+
+describe( 'get-color-scheme', function() {
+
+  describe( 'test this method', function() {
+
+    
+
+  } );
+
+
+  
+} );
---
+++ test/unit_tests/utils/tile-map_test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var chai = require( 'chai' );
+var expect = chai.expect;
+var tileMap =
+  require( '../../../src/static/js/utils/tile-map' );
+
+describe( 'tile-map', function() {
+
+  var getColor = tileMap.getColor;
+
+  describe( 'getColor', function() {
+
+
+  } );
+
+  
+} );

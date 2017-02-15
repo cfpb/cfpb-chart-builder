af89dbd3a7eb57e17bfe92acc4ba56d4829af7c5
Catherine Farman <Catherine.Farman@cfpb.gov>
2017-02-15 18:25:10 -0500

    add tile map color unit test

--- test/unit_tests/utils/tile-map_test.js
+++ test/unit_tests/utils/tile-map_test.js
@@ -11,8 +11,16 @@ describe( 'tile-map', function() {
 
   describe( 'getColor', function() {
 
+    it( 'should return the correct color HEX code for any given number', function() {
+      expect( getColor( -200 ) ).to.equal( '#96c4ed' );
+      expect( getColor( -6 ) ).to.equal( '#d6e8fa' );
+      expect( getColor( -5 ) ).to.equal( '#f7f8f9' );
+      expect( getColor( 0 ) ).to.equal( '#f7f8f9' );
+      expect( getColor( 10 ) ).to.equal( '#e2efd8' );
+      expect( getColor( 20 ) ).to.equal( '#bae0a2' );
+      expect( getColor( 200 ) ).to.equal( '#bae0a2' );
+    } );
 
   } );
-
   
 } );

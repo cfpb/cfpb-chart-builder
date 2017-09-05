08933983a13e3e8fe08a68d2b37f600af9fe9705
Chris Contolini <chris@contolini.com>
2017-09-05 11:49:15 -0400

    Update choropleth map colors

--- src/js/utils/color-range.js
+++ src/js/utils/color-range.js
@@ -5,36 +5,72 @@ const colors = {
     {
       from: 0,
       to: 0,
-      color: '#DCDDDF'
+      color: '#dcdddf'
     },
     {
       from: 0,
       to: 1,
-      color: '#D6E8FA'
+      color: '#d6e8fa'
     },
     {
       from: 1,
       to: 2,
-      color: '#AFD2F2'
+      color: '#afd2f2'
     },
     {
       from: 2,
       to: 3,
-      color: '#7EB7E8'
+      color: '#7eb7e8'
     },
     {
       from: 3,
       to: 4,
-      color: '#4497DC'
+      color: '#4497dc'
     },
     {
       from: 4,
       to: 5,
-      color: '#0072CE'
+      color: '#0072ce'
     },
     {
       from: 5,
-      color: '#0050B4'
+      color: '#0050b4'
+    }
+  ],
+  navy: [
+    {
+      from: 0,
+      to: 0,
+      color: '#dcdddf'
+    },
+    {
+      from: 0,
+      to: 1,
+      color: '#d3daeb'
+    },
+    {
+      from: 1,
+      to: 2,
+      color: '#b3c0d9'
+    },
+    {
+      from: 2,
+      to: 3,
+      color: '#889cc0'
+    },
+    {
+      from: 3,
+      to: 4,
+      color: '#5674a3'
+    },
+    {
+      from: 4,
+      to: 5,
+      color: '#254b87'
+    },
+    {
+      from: 5,
+      color: '#002d72'
     }
   ]
 };

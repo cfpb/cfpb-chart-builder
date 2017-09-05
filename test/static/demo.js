1009673b6f843e9bf24a9e8dfad2fa9c67c51065
Chris Contolini <chris@contolini.com>
2017-09-05 14:35:34 -0400

    Update browser smoke tests for CI environments

--- test/static/demo.js
+++ test/static/demo.js
@@ -33,34 +33,40 @@ const interval = setInterval( () => {
   clearInterval( interval );
 }, 1000 );
 
-setTimeout( () => {
-  chart.update({
-    source: 'mortgage-performance/time-series/30-89/12031;mortgage-performance/time-series/30-89/national',
-    metadata: 'pct90'
-  });
-  map.update({
-    source: 'mortgage-performance/map-data/30-89/counties/2009-01',
-    metadata: 'counties'
-  });
-}, 5000 );
+const updateAllTheCharts = () => {
+  setTimeout( () => {
+    chart.update({
+      source: 'mortgage-performance/time-series/30-89/12031;mortgage-performance/time-series/30-89/national',
+      metadata: 'pct90'
+    });
+    map.update({
+      source: 'mortgage-performance/map-data/30-89/counties/2009-01',
+      metadata: 'counties'
+    });
+  }, 5000 );
 
-setTimeout( () => {
-  map.update({
-    source: 'mortgage-performance/map-data/30-89/states/2009-01',
-    metadata: 'states'
-  });
-}, 10000 );
+  setTimeout( () => {
+    map.update({
+      source: 'mortgage-performance/map-data/30-89/states/2009-01',
+      metadata: 'states'
+    });
+  }, 10000 );
 
-setTimeout( () => {
-  map.update({
-    source: 'mortgage-performance/map-data/30-89/counties/2009-01',
-    metadata: 'counties'
-  });
-}, 15000 );
+  setTimeout( () => {
+    map.update({
+      source: 'mortgage-performance/map-data/30-89/counties/2009-01',
+      metadata: 'counties'
+    });
+  }, 15000 );
 
-setTimeout( () => {
-  map.update({
-    source: 'mortgage-performance/map-data/30-89/metros/2009-01',
-    metadata: 'metros'
-  });
-}, 20000 );
+  setTimeout( () => {
+    map.update({
+      source: 'mortgage-performance/map-data/30-89/metros/2009-01',
+      metadata: 'metros'
+    });
+  }, 20000 );
+};
+
+// Sauce Labs only runs the tests for ten seconds so don't test all
+// the chart updating stuff if we're in a CI environment.
+if (!isCI()) updateAllTheCharts();

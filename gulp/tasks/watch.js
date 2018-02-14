3d67d6fe6696471c3dbb88004e86d9b9013ded0a
Chris Contolini <chris@contolini.com>
2018-02-14 14:26:49 -0500

    Update gulp watch task to be gulp v4 compliant
    Version 4 of gulp has a new API
    
    https://github.com/gulpjs/gulp/blob/4.0/docs/API.md#gulpwatchglobs-opts-fn
    https://medium.com/@andersco/setting-up-browsersync-with-gulp-4-and-express-6bbb52244ffc

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,10 +5,34 @@
 
 const gulp = require( 'gulp' );
 const config = require( '../config' );
+const browserSync = require( 'browser-sync' ).create();
 
-gulp.task( 'watch', gulp.series( 'connect', 'browsersync' ), function() {
-  gulp.watch( [ config.test.src, config.demoScripts.src ], [ 'scripts:concat', 'scripts:demo', 'test:unit' ] );
-  gulp.watch( config.styles.cwd + config.styles.src, [ 'styles' ] );
-  gulp.watch( config.test.unit, [ 'test:unit' ] );
-  gulp.watch( config.demoStyles.cwd + config.demoStyles.src, [ 'styles' ] );
+gulp.task( 'watch:scripts', function( done ) {
+  gulp.watch( [ config.test.src, config.demoScripts.src ], gulp.series( 'scripts:concat', 'scripts:demo', 'test:unit' ) );
+  done();
 } );
+
+gulp.task( 'watch:styles', function( done ) {
+  gulp.watch( [ config.styles.cwd + config.styles.src, config.demoStyles.cwd + config.demoStyles.src ], gulp.series( 'styles' ) );
+  done();
+} );
+
+gulp.task( 'watch:tests', function( done ) {
+  gulp.watch( config.test.unit, gulp.series( 'test:unit' ) );
+  done();
+} );
+
+
+/**
+ * browserSyncInit - Initialize browser sync
+ *
+ * @param {type} done Async callback
+ */
+function browserSyncInit( done ) {
+  browserSync.init( config.browserSync );
+  done();
+}
+
+gulp.task( 'browser-sync', browserSyncInit );
+
+gulp.task( 'watch', gulp.parallel( 'watch:scripts', 'watch:styles', 'watch:tests', 'connect', 'browser-sync' ) );

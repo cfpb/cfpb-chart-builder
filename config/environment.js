3f4c2790c3ef16a73c48b61441b34012beb2a122
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-08-08 11:58:41 -0400

    Change envvar name

--- config/environment.js
+++ config/environment.js
@@ -9,12 +9,12 @@
 const envvars = {
 
   /* eslint-disable no-process-env */
+  NODE_ENV: process.env.NODE_ENV
   SAUCE_LABS_USERNAME: process.env.SAUCE_LABS_USERNAME,
   SAUCE_LABS_ACCESS_KEY: process.env.SAUCE_LABS_ACCESS_KEY,
   TRAVIS_PULL_REQUEST: process.env.TRAVIS_PULL_REQUEST,
   TRAVIS_PULL_REQUEST_BRANCH: process.env.TRAVIS_PULL_REQUEST_BRANCH,
   TRAVIS_JOB_NUMBER: process.env.TRAVIS_JOB_NUMBER,
-  CI_ENVIRONMENT: process.env.CI_ENVIRONMENT
   /* eslint-enable no-process-env */
 };
 

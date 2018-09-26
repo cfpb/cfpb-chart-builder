609458d26373c6f1ed9544a080e8ba17e8dab34d
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-09-26 17:23:38 -0400

    Update to babel 7

--- config/webpack-config.js
+++ config/webpack-config.js
@@ -10,15 +10,16 @@ const envvars = require( './environment' ).envvars;
    Runs code through Babel and uses global supported browser list. */
 const COMMON_MODULE_CONFIG = {
   rules: [ {
+    exclude: [/node_modules/],
     use: {
       loader: 'babel-loader?cacheDirectory=true',
       options: {
-        presets: [ [ 'babel-preset-env', {
+        presets: [ [ '@babel/preset-env', {
           configPath: __dirname,
           /* Use useBuiltIns: 'usage' and set `debug: true` to see what
              scripts require polyfilling. */
-          useBuiltIns: false,
-          debug: false
+          useBuiltIns: 'usage',
+          debug: true
         } ] ]
       }
     }
